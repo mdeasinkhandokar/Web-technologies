366a7ea90601d84413f8124c2385d81d7be6a987
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-07-19 23:14:17 +0600

    operation

---
+++ JavaScript/Operation/assignment.js
@@ -0,0 +1,10 @@
+let score= 10+11;
+
+let num1=10;
+
+num1 +=5;
+
+console.log(num1);
+
+let score1 = ((2*(3+2))-10);
+console.log(score1)
---
+++ JavaScript/Operation/operation.js
@@ -0,0 +1,29 @@
+let score=102;
+let bonus=25;
+
+let totalScore =score + bonus;
+
+
+let addition =4+4;
+let subtract= 9-3;
+let mult= 3*5;
+let div =8/2;
+let reminder=9/2;
+let expo=2**3;
+
+let myscore=110;
+myscore++;
+
+
+
+let credits=56;
+credits--;
+
+
+///comparion operation
+
+let num1=3;
+let num2=3;
+let num3=6;
+console.log(num1==num2);
+console.log(num1!=num3);
