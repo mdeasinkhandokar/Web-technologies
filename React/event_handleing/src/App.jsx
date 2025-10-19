b3f649069946a0c4b26cec0611a1719f3bb94edb
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-10-20 02:00:17 +0600

    Event Handling

---
+++ React/event_handleing/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+//   function handleClick(){
+//     alert("I am clicked");
+//   }
+
+// function handleMouseover(){
+//   alert("Easin Comming ");
+// }
+
+// function handleInputChange(e){
+//   // console.log("input me value change");
+//   console.log("Value till now: ", e.target.value);
+
+
+// }
+
+// function handleSubmit(e){
+//   e.preventDefult();
+//   alert("can i submit the from");
+// }
+
+
+
+
+  return (
+    
+    <div>
+
+
+{/* 
+     <button onClick={alert("Button click ")}> 
+      Click me
+     </button> */}
+
+
+
+
+
+
+
+
+
+
+    {/* <from onSubmit={handleSubmit}>
+      <input type='text' onChange={handleInputChange}/>
+      <button type="submit"> Submit</button>
+    </from> */}
+
+
+      
+      {/* <p onMouseOver={handleMouseover} style={{border:"1px solid black"}}>
+        I am Easin
+      </p>
+      */}
+    {/* <button onClick={handleClick}>
+      Click me
+    </button>      */}
+
+
+
+
+    </div>
+  )
+}
+
+export default App
